f8e2d046bbb2a8f1ba5b5489c68b3f9c273ef0a2
triccsr <triccsr@outlook.com>
2025-03-30 22:00:10 +0800

    torus

--- src/FunctionLayer/Shape/Triangle.cpp
+++ src/FunctionLayer/Shape/Triangle.cpp
@@ -1,4 +1,6 @@
 #include "Triangle.h"
+#include "CoreLayer/Math/Geometry.h"
+#include "FastMath/VecMat.h"
 #include <FunctionLayer/Acceleration/Linear.h>
 //--- Triangle ---
 Triangle::Triangle(int _primID, int _vtx0Idx, int _vtx1Idx, int _vtx2Idx,
@@ -17,6 +19,25 @@ Triangle::Triangle(int _primID, int _vtx0Idx, int _vtx1Idx, int _vtx2Idx,
 bool Triangle::rayIntersectShape(Ray &ray, int *primID, float *u,
                                  float *v) const {
   //* todo 实现三角形与光线求交
+
+  // transform ray WITHOUT SCALE
+  Point3f origin = ray.origin;
+  Vector3f direction = ray.direction;
+
+  // 求交点
+  Point3f V0 = mesh->transform.toWorld(mesh->meshData->vertexBuffer[vtx0Idx]),
+          V1 = mesh->transform.toWorld(mesh->meshData->vertexBuffer[vtx1Idx]),
+          V2 = mesh->transform.toWorld(mesh->meshData->vertexBuffer[vtx2Idx]);
+  Vector3f E1 = V1 - V0, E2 = V2 - V0, T = origin - V0;
+  Vector3f Q = cross(T, E1), P = cross(direction, E2);
+  float inv = 1.f / dot(P, E1);
+  float t = dot(Q, E2) * inv;
+  *u = dot(P, T) * inv, *v = dot(Q, direction) * inv;
+  if (*u >= 0 && *v >= 0 && *u + *v <= 1 && t >= ray.tNear && t <= ray.tFar) {
+    ray.tFar = std::min(ray.tFar, t);
+    *primID = this->primID;
+    return true;
+  }
   return false;
 }
 
@@ -69,10 +90,32 @@ void TriangleMesh::fillIntersection(float distance, int primID, float u,
   //* todo 填充光线与三角网格求交得到的交点信息
   intersection->distance = distance;
   intersection->shape = this;
+
+  Point3f
+      V0 = transform.toWorld(
+          meshData->vertexBuffer[meshData->faceBuffer[primID][0].vertexIndex]),
+      V1 = transform.toWorld(
+          meshData->vertexBuffer[meshData->faceBuffer[primID][1].vertexIndex]),
+      V2 = transform.toWorld(
+          meshData->vertexBuffer[meshData->faceBuffer[primID][2].vertexIndex]);
+  Vector3f E1 = V1 - V0, E2 = V2 - V0;
+
   //* 1. 在三角形内部用插值计算交点坐标
+  Point3f hitpoint = V0 + u * E1 + v * E2;
+  intersection->position = hitpoint;
+
   //* 2. 在三角形内部用插值计算法线
+  intersection->normal = normalize(cross(E1, E2));
+
   //* 3. 在三角形内部用插值计算纹理坐标
+  intersection->texCoord = Vector2f{u, v};
+
   //* 4. 在三角形内部用插值计算交点的切线和副切线
+  Vector3f tangent = E1;
+  Vector3f bitangent = normalize(cross(tangent, intersection->normal));
+  tangent = normalize(cross(intersection->normal, bitangent));
+  intersection->tangent = tangent;
+  intersection->bitangent = bitangent;
 }
 
 void TriangleMesh::initInternalAcceleration() {
